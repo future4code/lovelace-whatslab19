33a99e858a14ee5f9a0de2fa82bbcfb4f046ac44
Rafael Sônego <rasonego@gmail.com>
2021-07-16 15:48:48 -0300

    Criação - Borda central

---
+++ src/Card/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from './style'
+
+export class Card extends React.Component {
+    render() {
+        return (
+            <Container>
+                <div>
+                    <h3>WhatsLab</h3>
+                </div>
+            </Container>
+        );
+    }
+}
