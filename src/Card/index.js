55d3b2ff2c83cb130becf4e9fcd4a4022df90367
Rafael Sônego <rasonego@gmail.com>
2021-07-18 21:11:29 -0300

    Criação dos estados array de mensagem

--- src/Card/index.js
+++ src/Card/index.js
@@ -4,12 +4,33 @@ import { Container } from './style'
 
 export class Card extends React.Component {
 
+    state = {
+        mensagens: []
+    }
+
+    show = (mensagem, usuario) => {
+        this.setState({ mensagens: [...this.state.mensagens, { mensagem, usuario }] })
+        console.log(mensagem, usuario, this.state.mensagens, "MOSTRANDO")
+    }
 
     render() {
         return (
             <Container>
                 <h3>WhatsLab</h3>
-                <Footer/>
+                {this.state.mensagens.map((mensagem) => {
+
+                    return (
+                        <div>
+                            <p>
+                                {mensagem.usuario}
+                            </p>
+                            <p>
+                                {mensagem.mensagem}
+                            </p>
+                        </div>
+                    )
+                })}
+                <Footer show={this.show} />
             </Container>
         );
     }
--- src/Footer/index.js
+++ src/Footer/index.js
@@ -8,20 +8,37 @@ export class Footer extends React.Component {
         mensagem: "",
     };
 
+    changeUsuario = (event) => {
+        this.setState({
+            usuario: event.target.value
+        });
+    };
+
+    changeMensagem = (event) => {
+        this.setState({
+            mensagem: event.target.value
+        });
+    };
+
+    tela = () => {
+        
+        this.props.show(this.state.mensagem , this.state.usuario)
+    }
+
     render() {
         return (
             <ContainerFooter>
                 <input
                     placeholder={"Usuário"}
                     value={this.state.usuario}
-                    // onChange={}
+                    onChange={this.changeUsuario}
                 />
                 <input
                     placeholder={"Mensagem"}
                     value={this.state.mensagem}
-                    // onChange={}
+                    onChange={this.changeMensagem}
                 />
-                <button>Enviar</button>
+                <button onClick={this.tela}>Enviar</button>
             </ContainerFooter>
         )
     }
